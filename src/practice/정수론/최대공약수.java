dd485e6a40d787d2eed1003af9537a8774cd755c
KimHaKyeon95 <gkrgus0810@gmail.com>
2024-01-08 14:17:23 +0900

    최대공약수: https://www.acmicpc.net/problem/1850

--- src/practice/정수론/최대공약수.java
+++ src/practice/정수론/최대공약수.java
@@ -8,6 +8,7 @@ public class 최대공약수 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         long A = sc.nextLong();
+
         long B = sc.nextLong();
 
         if (A > B) {
