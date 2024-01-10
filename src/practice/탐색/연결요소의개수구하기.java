4931b346fa629d1f35fc766cff7e350cffb08917
KimHaKyeon95 <gkrgus0810@gmail.com>
2024-01-10 22:55:26 +0900

    연결요소의개수구하기 복습

--- src/practice/탐색/연결요소의개수구하기.java
+++ src/practice/탐색/연결요소의개수구하기.java
@@ -14,38 +14,41 @@ public class 연결요소의개수구하기 {
         int node = Integer.parseInt(st.nextToken());
         int edge = Integer.parseInt(st.nextToken());
 
-        visited = new boolean[node+1];
         A = new ArrayList[node+1];
-        for(int i = 1; i < node+1; i++) {
+        visited = new boolean[node+1];
+
+        for (int i = 1; i < node+1; i++) {
             A[i] = new ArrayList<>();
         }
 
         for (int i = 0; i < edge; i++) {
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            A[start].add(end);
-            A[end].add(start);
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            A[u].add(v);
+            A[v].add(u);
         }
+
         int count = 0;
         for (int i = 1; i < node+1; i++) {
-            if(!visited[i]) {
+            if (!visited[i]) {
                 count++;
-                dfs(i);
+                dfs(A[i]);
+            }
+        }
+        System.out.println(count);
+
+    }
+
+    private static void dfs(ArrayList a) {
+        for (int i = 0; i < a.size(); i++) {
+            if (!visited[(int) a.get(i)]) {
+                visited[(int) a.get(i)] = true;
+                dfs(A[(int) a.get(i)]);
             }
         }
     }
 
-    static void dfs(int node) {
-        if (visited[node]) {
-            return;
-        }
-        visited[node] = true;
-        for (int i : A[node]) {
-            if (!visited[i]) {
-                dfs(i);
-            }
-        }
-    }
 
 }
