74485996436b9bebe2d663437172c5841224eb7b
KimHaKyeon95 <gkrgus0810@gmail.com>
2024-02-15 23:27:46 +0900

    트리 재풀이: https://www.acmicpc.net/problem/1068

--- src/practice/그래프/트리/트리.java
+++ src/practice/그래프/트리/트리.java
@@ -1,56 +1,64 @@
 package practice.그래프.트리;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
+import jdk.internal.org.objectweb.asm.tree.ParameterNode;
 
 public class 트리 {
 
-    static int n, delete;
-    static int[] parent;
-    static int count;
-    static boolean[] visited;
+    private static ArrayList<Integer>[] edges;
+    private static int answer = 0;
+    private static int deleteNode = 0;
+    private static boolean[] visited;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        n = scan.nextInt();
-        parent = new int[n];
+        int N = Integer.parseInt(st.nextToken());
+        edges = new ArrayList[N];
+        for (int i = 0; i < N; i++) {
+            edges[i] = new ArrayList<>();
+        }
+        visited = new boolean[N];
         int root = 0;
-        for(int i = 0; i < n; i++) {
-            parent[i] = scan.nextInt();
-            if(parent[i] == -1) root = i;
-        }
-        delete = scan.nextInt();
 
-        deleteNode(delete);
-
-        count = 0;
-        visited = new boolean[n];
-        countLeaf(root);
-
-        System.out.println(count);
-    }
-
-    public static void deleteNode(int d) {
-        parent[d] = -2; //삭제된 노드 -2로 표시
-        for(int i = 0; i < n; i++) {
-            if(parent[i] == d) {
-                deleteNode(i);
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int node = Integer.parseInt(st.nextToken());
+            if (node != -1) {
+                edges[i].add(node);
+                edges[node].add(i);
+            } else {
+                root = i;
             }
         }
-    }
 
-    public static void countLeaf(int s) {
-        boolean isLeaf = true;
-        visited[s] = true;
-        if(parent[s] != -2) {
-            for(int i = 0; i < n; i++) {
-                if(parent[i] == s && visited[i] == false) {
-                    countLeaf(i);
-                    isLeaf = false;
-                }
-            }
-            if(isLeaf) count++;
+        st = new StringTokenizer(br.readLine());
+        deleteNode = Integer.parseInt(st.nextToken());
+
+        if(deleteNode == root) {
+            System.out.println(0);
+        } else {
+            dfs(root);
+            System.out.println(answer);
         }
     }
+
+
+    private static void dfs(int a) {
+        visited[a] = true;
+        int cNode = 0;
+        for (int i : edges[a]) {
+            if (!visited[i] && i != deleteNode) {
+                cNode++;
+                dfs(i);
+            }
+        }
+        if (cNode == 0) {
+            answer++;
+        }
+    }
+
 }
 
