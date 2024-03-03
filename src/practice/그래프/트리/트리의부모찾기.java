940380a1be8319a17ca7233ea258632f1e8b08b2
jack.khh(김학현)/dkt <jack.khh@kakaocorp.com>
2024-03-03 22:15:32 +0900

    트리의부모찾기 재풀이: https://www.acmicpc.net/problem/11725

--- src/practice/그래프/트리/트리의부모찾기.java
+++ src/practice/그래프/트리/트리의부모찾기.java
@@ -11,10 +11,9 @@ public class 트리의부모찾기 {
     private static int[] parents;
 
     public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        Scanner sc = new Scanner(System.in);
 
-        int N = Integer.parseInt(st.nextToken());
+        int N = sc.nextInt();
         edges = new ArrayList[N+1];
         visited = new boolean[N+1];
         parents = new int[N+1];
@@ -24,30 +23,30 @@ public class 트리의부모찾기 {
         }
 
         for (int i = 1; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+
+            int a = sc.nextInt();
+            int b = sc.nextInt();
 
             edges[a].add(b);
             edges[b].add(a);
         }
 
-        dfs(1);
+        Queue<Integer> queue = new LinkedList<Integer>();
+        visited[1] = true;
+        queue.add(1);
+
+        while(!queue.isEmpty()) {
+            int node = queue.poll();
+            visited[node] = true;
+            for (int i : edges[node]) {
+                if (!visited[i]) {
+                    queue.add(i);
+                    parents[i] = node;
+                }
+            }
+        }
         for (int i = 2; i < N+1; i++) {
             System.out.println(parents[i]);
-        }
-    }
-
-    private static void dfs(int a) {
-        if (visited[a]) {
-            return;
-        }
-        visited[a] = true;
-        for (int i : edges[a]) {
-            if (!visited[i]) {
-                parents[i] = a;
-                dfs(i);
-            }
         }
     }
 }
